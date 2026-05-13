2bda6126864e441b9a68c9f101b1242278f10fc0
AstralForgeStudios <scribe@astralforge.studio>
2026-05-12 20:08:44 -0500

    Delete All

--- .next/dev/static/chunks/src_app_layout_tsx_004glpo._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_app_globals_0p2ml0n.css","static/chunks/node_modules_next_09h51ly._.js","static/chunks/src_05~9f74._.js"],
-    source: "dynamic"
-});
